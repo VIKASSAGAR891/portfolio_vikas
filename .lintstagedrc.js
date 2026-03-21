225e772a2c75c16766c84fdef5d16d413e497788
VIKASSAGAR891 <vikassagar891@gmail.com>
2026-03-21 14:33:52 +0530

    updated

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,20 +1,4 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const buildEslintCommand = filenames =>
-  `eslint --fix ${filenames
-    .map(f => path.relative(process.cwd(), f))
-    .join(' ')}`;
-
-
 export default {
-  'app/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
-  'components/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
-  'pages/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
-
-  '*.{css,postcss}': 'stylelint --fix',
-  '*.styled.{js,jsx}': 'stylelint --fix',
+  '**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
+  '**/*.{css,postcss}': ['stylelint --fix'],
 };
